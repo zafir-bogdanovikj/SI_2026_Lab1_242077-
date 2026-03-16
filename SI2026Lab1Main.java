1b3eb9f4d5efb804f9c4cdc6f96492642beb88e1
Zafir Bogdanovikj <zafirbogdanovikj@Mac-8.home>
2026-03-16 13:24:27 +0100

    Implement book search

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -53,6 +53,11 @@ class Library {
 
     // TODO: Implement in branch feature-search-books
     public boolean searchBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -120,5 +125,7 @@ public class SI2026Lab1Main {
         library.addBook(new Book("1984", "George Orwell", "Dystopian"));
 
         System.out.println("Library initialized.");
-    }
+        System.out.println(library.searchBookByTitle("Testing feature-search-books"));        
+   
+ }
 }
